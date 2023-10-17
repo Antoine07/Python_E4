ef76dad62f73a340d4d944bb346a01d2b68a48af
Antoine07 <antoine.lucsko@gmail.com>
2023-10-17 17:14:08 +0200

    correction

--- Part1_Introduction/Exercices/008_comprehension_imbrique.py
+++ Part1_Introduction/Exercices/008_comprehension_imbrique.py
@@ -1,10 +1,14 @@
 
 r = []
 
-for i in range(1, 5):
+for i in range(1, 3):
     for j in range(1, 5):
         r.append((i, j))
 
 print(r)
 
 # Transofmer cela en compréhention de liste
+
+r = [ (i, j) for i in range(1, 3) for j in range(1, 5) ]
+
+print(r)
