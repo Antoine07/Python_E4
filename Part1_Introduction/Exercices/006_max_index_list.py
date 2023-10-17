aeae534a8b90e74d8cc9026dcb9fd2bd74beeeff
Antoine07 <antoine.lucsko@gmail.com>
2023-10-17 15:23:17 +0200

    max liste

--- Part1_Introduction/Exercices/006_max_index_list.py
+++ Part1_Introduction/Exercices/006_max_index_list.py
@@ -6,13 +6,16 @@ Cette fonction retourne le max et son indice dans la liste de nombre
 l = [0, 1, 100, 4, 9, 16, 25, 36, 49, 64, 81]   
 
 def maxList(l):
+    assert type(l) == list and len(l) > 0, "error type list or empty list"
+
     m, j = l[0], 0
     for i in range(1, len(l)) :
         if l[i] > m :
             m, j = l[i], i
 
-
     return { 'max' : m, 'indice' : j }
 
-    
-print(maxList(l))
+try:
+    print(maxList([]))
+except AssertionError as e:
+    print(e)
