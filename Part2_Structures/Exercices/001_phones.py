99d1fe4c0ff98b0a191b08b8f24b37a7f3da8e71
Antoine07 <antoine.lucsko@gmail.com>
2023-10-18 12:12:25 +0200

    correction phones

--- Part2_Structures/Exercices/001_phones.py
+++ Part2_Structures/Exercices/001_phones.py
@@ -1,3 +1,5 @@
+# 1. Nettoyez les données, vous retirez l'item EURO pour le prix de chaque appareil. 
+print("------ première correction ------")
 
 phones = [
     { 'name': "iphone XX", 'priceHT': "900EURO" },
@@ -9,14 +11,18 @@ phones = [{'name': phone['name'], 'priceHT': float( phone['priceHT'].replace('EU
 
 print(phones)
 
+# une liste de tuples
+phones = [ (phone['name'], phone['priceHT']) for phone in phones ]
 
-print("--------")
+print(phones)
 
-phones = [
-    { 'name': "iphone XX", 'priceHT': "900EURO" },
-    { 'name': "iphone X", 'priceHT': "70EURO" },
-    { 'name': "iphone B", 'priceHT': "200EURO" },
-]
+print("------ deuxième correction ------")
+
+# phones = [
+#     { 'name': "iphone XX", 'priceHT': "900EURO" },
+#     { 'name': "iphone X", 'priceHT': "70EURO" },
+#     { 'name': "iphone B", 'priceHT': "200EURO" },
+# ]
 
 # def sanitizeEuro(phones):
 
@@ -28,7 +34,9 @@ phones = [
 # print(phones)
 
 # le map il itére sur chaque valeur de la liste, une logique pour un item de la liste
-def sanitizeEuro(phone):
-    return { 'name' : phone['name'] , 'priceHT' : float( phone['priceHT'].replace('EURO', '') ) }
+# def sanitizeEuro(phone):
+#     return { 'name' : phone['name'] , 'priceHT' : float( phone['priceHT'].replace('EURO', '') ) }
 
-print( list(map(sanitizeEuro, phones)) )
+# print( list(map(sanitizeEuro, phones)) )
+
+# 2. Créez des tuples à partir des dictionnaires (changement de structure de données)
